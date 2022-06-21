c2d92d8c7edf0e518466e1f8c1a5449d1731bed0
Beta Zhou <zhoupin@nwcdcloud.cn>
2022-06-21 12:33:31 +0800

    add glue override

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -itd -p 8888:8888 -p 4040:4040 -v ~/.aws:/home/glue_user/.aws:ro -v $(pwd)/conf/core-site.xml:/home/glue_user/spark/conf/core-site.xml --name glue_jupyter amazon/aws-glue-libs:glue_libs_3.0.0_image_01 /home/glue_user/jupyter/jupyter_start.sh
+docker run -itd -p 8888:8888 -p 4040:4040 \
+    -v ~/.aws:/home/glue_user/.aws:ro \
+    -v $(pwd)/conf/core-site.xml:/home/glue_user/spark/conf/core-site.xml \
+    -v $(pwd)/conf/glue-default.conf:/home/glue_user/spark/conf/cglue-default.conf \
+    -v $(pwd)/conf/glue-override.conf:/home/glue_user/spark/conf/glue-override.conf \
+    --name glue_jupyter amazon/aws-glue-libs:glue_libs_3.0.0_image_01 /home/glue_user/jupyter/jupyter_start.sh
