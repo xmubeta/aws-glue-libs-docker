8510c839c7fe67641e4445a410cb4da494143e21
Beta Zhou <zhoupin@nwcdcloud.cn>
2022-07-12 12:19:10 +0800

    add mount volume for workspace add spark history server port map

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker run -itd -p 8888:8888 -p 4040:4040 \
+docker run -itd -p 8888:8888 -p 4040:4040 -p 18080:18080 \
     -v ~/.aws:/home/glue_user/.aws:ro \
+    -v $(pwd)/workspace:/home/glue_user/workspace/jupyter_workspace \
     -v $(pwd)/conf/core-site.xml:/home/glue_user/spark/conf/core-site.xml \
     -v $(pwd)/conf/glue-default.conf:/home/glue_user/spark/conf/glue-default.conf \
     -v $(pwd)/conf/glue-override.conf:/home/glue_user/spark/conf/glue-override.conf \
